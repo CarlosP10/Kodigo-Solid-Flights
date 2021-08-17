6acaab63bd0ca465b7bb7894968081a12016657a
notkooru <notkooru@gmail.com>
2021-08-17 13:38:04 -0600

    Created Aircraft class attributes

--- src/main/java/com/kodigo/models/Aircraft.java
+++ src/main/java/com/kodigo/models/Aircraft.java
@@ -1,4 +1,9 @@
 package com.kodigo.models;
 
 public class Aircraft {
+    private String aircraft;
+    private String airline;
+    private int capacity;
+    private String model;
+    private int flyDistance;
 }
