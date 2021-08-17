05dba5cd55a175e0da9c63d8160ae15774e382ac
notkooru <notkooru@gmail.com>
2021-08-17 13:57:46 -0600

    Created aircraft constructor using lombok

--- src/main/java/com/kodigo/models/Aircraft.java
+++ src/main/java/com/kodigo/models/Aircraft.java
@@ -1,9 +1,22 @@
 package com.kodigo.models;
 
+import lombok.*;
+
+@Getter
+@Setter
 public class Aircraft {
     private String aircraft;
     private String airline;
     private int capacity;
     private String model;
     private int flyDistance;
+
+    public Aircraft(){
+
+    }
+
+    public Aircraft(String airline, String model){
+        this.airline = airline;
+        this.model = model;
+    }
 }
