364d43e001ef472d85034b9f594029c328f2a597
hector994 <50502676+hector994@users.noreply.github.com>
2021-08-17 20:50:44 -0600

    testing lombok utilities and github funcionalities

---
+++ src/main/java/com/kodigo/models/Main.java
@@ -0,0 +1,29 @@
+package com.kodigo.models;
+
+import lombok.NonNull;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Weather clima = new Weather(27,false);
+        System.out.println(clima);
+        clima.setTemperature(67.6);
+        var hola = clima.toString();
+        System.out.println(hola);
+
+        var nombre = new ArrayList<String>();
+        hola("h");
+        clima.setPrecipitation(true);
+        System.out.println("Esta lloviendo?");
+        var lloviendo = clima.isPrecipitation();
+        System.out.println(lloviendo);
+    }
+    public static void hola(@NonNull String obj){
+        if (obj.contains("f")){
+            throw new NullPointerException("come null");
+        }
+        System.out.println( obj);
+
+    }
+}
