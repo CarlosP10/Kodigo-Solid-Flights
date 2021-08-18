e6fe5cde3010bb5a4643b1ab6121e2a9584f73c8
hector994 <50502676+hector994@users.noreply.github.com>
2021-08-18 13:59:22 -0600

    Aplicando lombok a las clases

--- src/main/java/com/kodigo/models/Aircraft.java
+++ src/main/java/com/kodigo/models/Aircraft.java
@@ -4,18 +4,21 @@ import lombok.*;
 
 @Getter
 @Setter
+@Data
+@NoArgsConstructor
 public class Aircraft {
     private int capacity;
     private String model;
     private double flyDistance;
+//not need thank to lombok
+/*    public Aircraft(){
 
-    public Aircraft(){
+    }*/
 
-    }
-
-    public Aircraft(String model, int capacity, double flyDistance){
+//not need thank to lombok
+/*    public Aircraft(String model, int capacity, double flyDistance){
         this.model = model;
         this.capacity = capacity;
         this.flyDistance = flyDistance;
-    }
+    }*/
 }
--- src/main/java/com/kodigo/models/FlightTo.java
+++ src/main/java/com/kodigo/models/FlightTo.java
@@ -3,16 +3,22 @@ package com.kodigo.models;
 import lombok.*;
 
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FlightTo {
         private String city;
         private String country;
-
+/*
+        //se aplica lombok ya no es necdsario declararlo
         public FlightTo(){
 
-        }
+        }*/
 
+/*
+        //ya no es necesario gracias a lombok
         public FlightTo(String city, String country){
             this.city = city;
             this.country = country;
-    }
+    }*/
 }
