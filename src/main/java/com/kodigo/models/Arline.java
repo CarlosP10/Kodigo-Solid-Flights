8a2216b761d0783d12f4a913dbdba7056fd83287
hector martinez <mejia.martinez2012@hotmail.es>
2021-08-17 22:54:40 -0600

    Created Airline class

---
+++ src/main/java/com/kodigo/models/Arline.java
@@ -0,0 +1,17 @@
+package com.kodigo.models;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+public class Arline {
+    private String arlineName;
+
+    public Arline(){
+
+    }
+    public Arline(String arlineName){
+        this.arlineName = arlineName;
+    }
+}
