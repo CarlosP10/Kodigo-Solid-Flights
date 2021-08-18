c83e570c879e923d8d75df538a8e1f31c4dba02e
hector994 <50502676+hector994@users.noreply.github.com>
2021-08-17 20:48:04 -0600

    testing lombok utilities and github funcionalities

--- src/main/java/com/kodigo/models/Weather.java
+++ src/main/java/com/kodigo/models/Weather.java
@@ -1,4 +1,13 @@
 package com.kodigo.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Weather {
+    private double temperature;
+    private boolean precipitation;
 }
