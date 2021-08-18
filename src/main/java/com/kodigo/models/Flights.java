72d17d6bc5759a7c5fe637d258ce37260272ba3f
hector994 <50502676+hector994@users.noreply.github.com>
2021-08-18 13:49:09 -0600

    Creating the Flights class...

--- src/main/java/com/kodigo/models/Flights.java
+++ src/main/java/com/kodigo/models/Flights.java
@@ -2,6 +2,10 @@ package com.kodigo.models;
 
 public class Flights {
     private String ID;
+    private String postalCode;
     Aircraft aircraft;
-
+    //FlightInformation flightInformation;
+    FlightTo flightTo;
+    //Airline airline;
+    private boolean validate;
 }
