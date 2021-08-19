443cb1a74572ad0b46a7715d1e38b0935a06ad55
hector994 <50502676+hector994@users.noreply.github.com>
2021-08-18 18:34:01 -0600

    Aplicando lombok a las clases

--- src/main/java/com/kodigo/models/Aircraft.java
+++ src/main/java/com/kodigo/models/Aircraft.java
@@ -10,12 +10,12 @@ public class Aircraft {
     private int capacity;
     private String model;
     private double flyDistance;
-//not need thank to lombok
+//not needed thank to lombok
 /*    public Aircraft(){
 
     }*/
 
-//not need thank to lombok
+//not needed thank to lombok
 /*    public Aircraft(String model, int capacity, double flyDistance){
         this.model = model;
         this.capacity = capacity;
