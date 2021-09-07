491cbbd1fc87f7a53baaad9f171c62bc5429a577
hector994 <50502676+hector994@users.noreply.github.com>
2021-09-06 19:03:01 -0600

    poder enviar archivos por mail enviando imagenes
    se ha modificado la clase Mail de la rama developer para que ahora se puedan enviar archivos como imagenes o archivos docs que se encuentren en un directorio el directorio de prueba esta en la raiz del proyecto la imagen se llama Ghosts.jpg es un exelente wallpaper
    en conclusion ahora ya se pueden enviar archivos desde la clase Mail

--- src/main/java/com/kodigo/Mail.java
+++ src/main/java/com/kodigo/Mail.java
@@ -1,9 +1,14 @@
 package com.kodigo;
 
 import javax.mail.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 public class Mail {
 
     public static void sendMail(String recepient) {
@@ -38,7 +43,28 @@ public class Mail {
             message.setFrom(new InternetAddress(myAccountEmail));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
             message.setSubject("Email test");
-            message.setText("this is a test email from java");
+
+            //se crea el cuerpo del mensaje
+            Multipart multipart = new MimeMultipart();//instancia del cuerpo del mensaje
+            MimeBodyPart attachmentPart = new MimeBodyPart();// recibe cualquier archivo
+            MimeBodyPart textPart = new MimeBodyPart();// texto del mensaje
+            try {
+                /**
+                 * Puedes modificar la ruta del archivo que se almacenara en "f" a cualquier otro archivo,
+                 * pero recomiendo que utilicemos el directorio del proyecto como prueba es decir
+                 * que se coloquen los archivos en Kodigo-Solid-Flights/archivo.extencion
+                 * para propositos de prueba he dejado una imagen Ghosts.jpg que es un excelente wallpaper
+                 * para los que conocen linux. Espero que les guste este wallpaper
+                 */
+                File f =new File("/home/hector/IdeaProjects/Kodigo-Solid-Flights/Ghosts.jpg");
+                attachmentPart.attachFile(f);
+                textPart.setText("Hey! "+recepient+" this are all the flights you enter in the shell.");
+                multipart.addBodyPart(textPart);
+                multipart.addBodyPart(attachmentPart);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            message.setContent(multipart);
             return message;
         } catch (MessagingException e) {
             e.printStackTrace();
